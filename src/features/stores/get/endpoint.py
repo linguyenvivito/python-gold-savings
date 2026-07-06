ff83ab264eabc21e0c827e70e334cd0b13fc7504
Li Nguyen <nguyenminhtuanitc@gmail.com>
2026-07-07 05:19:13 +1000

    fix: Add image data for store

--- src/features/stores/get/endpoint.py
+++ src/features/stores/get/endpoint.py
@@ -10,7 +10,7 @@ def health_check():
 def get_stores():
     # Placeholder for actual store retrieval logic
     stores = [
-        {"id": 1, "code": "SJC", "name": "SJC", "address": "123 Main St", "phone": "123-456-7890", "culture": "vi-VN", "note": "Note A"},
-        {"id": 2, "code": "PNJ", "name": "PNJ", "address": "456 Elm St", "phone": "987-654-3210", "culture": "vi-VN", "note": "Note B"},
+        {"id": 1, "code": "SJC", "name": "SJC", "image": "https://static.vecteezy.com/system/resources/previews/023/654/784/non_2x/golden-logo-template-free-png.png", "address": "123 Main St", "phone": "123-456-7890", "culture": "vi-VN", "note": "Note A"},
+        {"id": 2, "code": "PNJ", "name": "PNJ", "image": "https://static.vecteezy.com/system/resources/previews/023/654/784/non_2x/golden-logo-template-free-png.png", "address": "456 Elm St", "phone": "987-654-3210", "culture": "vi-VN", "note": "Note B"},
     ]
     return stores
